fd6cab6f00d50e2de400ab1d58929a0d6ee79263
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2018-12-13 19:40:04 +0300

    Improved handling of video without exact duration.

--- ffprobe.py
+++ ffprobe.py
@@ -15,9 +15,26 @@ def get_resolution(path):
 def get_frames(path):
     for stream in _get_json(path)['streams']:
         if stream['codec_type'] == 'video':
-            return int(stream['nb_frames'])
+            if 'nb_frames' in stream:
+                return int(stream['nb_frames'])
 
 def get_duration(path):
     for stream in _get_json(path)['streams']:
         if stream['codec_type'] == 'video':
-            return float(stream['duration'])
+            if 'duration' in stream:
+                return float(stream['duration'])
+            else:
+                parts = stream['tags']['DURATION'].split(':')
+                assert len(parts) == 3
+                return float(parts[0]) * 3600 + float(parts[1]) * 60 + float(parts[2])
+
+def get_frame_rate(path):
+    for stream in _get_json(path)['streams']:
+        if stream['codec_type'] == 'video':
+            if 'avg_frame_rate' in stream:
+                assert stream['avg_frame_rate'].count('/') <= 1
+                parts = stream['avg_frame_rate'].split('/')
+                result = float(parts[0])
+                if len(parts) == 2:
+                    result /= float(parts[1])
+                return result
